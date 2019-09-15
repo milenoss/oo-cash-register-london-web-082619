422f9b3e4022dadaaad2e5aab55d695f0a28e5f7
Milen Khanal <milenkhanal@gmail.com>
2019-09-15 15:05:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,69 +1,30 @@
-require 'pry'
+
 
 class CashRegister
- attr_accessor :cart, :total  #returns total and cart. getters.
- attr_reader :discount   #gets the discount
-  
+attr_accessor :total, :cart
+  attr_reader :discount
+
   def initialize(discount = nil)
-    @discount = discount
-    @total = 0 
-    @@cart = [] 
+  @total = 0
+  @discount = discount
+  @cart = []
+
+  end
+
+  def add_item(title, price, quantity = 1)
+  quantity.times{ @cart << {title: title, price: price, quantity: quantity}}
+  @total += price * quantity
+  end
+
+  def apply_discount
+  if @discount
+      @total = @total *(100-@discount)/100
+      "After the discount, the total comes to $#{@total}."
+  else
+        "There is no discount to apply."
+  end
   end
   
-  def add_item(title, price, quantity = 1)
-    @total += quantity * price 
-   quantity.times{@cart << {title: title, price: price, quantity: quantity}}
-   end 
-   
-   def apply_discount
-    if @discount 
-      self.total += total*( 100 - @discount) /total
-      else 
-      "There is no discount to apply"
-    end
-  end
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class CashRegister
-# attr_accessor :total, :cart
-#   attr_reader :discount
-
-#   def initialize(discount = nil)
-#   @total = 0
-#   @discount = discount
-#   @cart = []
-
-#   end
-
-#   def add_item(title, price, quantity = 1)
-#   quantity.times{ @cart << {title: title, price: price, quantity: quantity}}
-#   @total += price * quantity
-#   end
-
-#   def apply_discount
-#   if @discount
-#       @total = @total *(100-@discount)/100
-#       "After the discount, the total comes to $#{@total}."
-#   else
-#         "There is no discount to apply."
-#   end
-#   end
 
 #   def items
 #     @cart.map{|items| items[:title]}
