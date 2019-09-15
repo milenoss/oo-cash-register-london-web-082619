68dffd455b5aee2224e7cb9563775c18d4cb0da1
Milen Khanal <milenkhanal@gmail.com>
2019-09-15 12:41:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,34 +1,61 @@
 
 class CashRegister
-attr_accessor :total, :cart
-   attr_reader :discount
-
-   def initialize(discount = nil)
-   @total = 0
-   @discount = discount
-   @cart = []
-
-   end
-
-   def add_item(title, price, quantity = 1)
-   quantity.times{ @cart << {title: title, price: price, quantity: quantity}}
-   @total += price * quantity
-   end
-
-  def apply_discount
-   if @discount
-       @total = @total *(100-@discount)/100
-       "After the discount, the total comes to $#{@total}."
-   else
-        "There is no discount to apply."
-   end
-   end
-
-   def items
-     @cart.map{|items| items[:title]}
-   end
-
-   def void_last_transaction
-       @total -= @cart.last[:price] * @cart.last[:quantity]
-   end
+  attr_reader :discount 
+  attr_accessor :car, :total
+  def initialization(discount = nil )
+  @discount = discount
+  @total = 0
+  @cart = []
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class CashRegister
+# attr_accessor :total, :cart
+#   attr_reader :discount
+
+#   def initialize(discount = nil)
+#   @total = 0
+#   @discount = discount
+#   @cart = []
+
+#   end
+
+#   def add_item(title, price, quantity = 1)
+#   quantity.times{ @cart << {title: title, price: price, quantity: quantity}}
+#   @total += price * quantity
+#   end
+
+#   def apply_discount
+#   if @discount
+#       @total = @total *(100-@discount)/100
+#       "After the discount, the total comes to $#{@total}."
+#   else
+#         "There is no discount to apply."
+#   end
+#   end
+
+#   def items
+#     @cart.map{|items| items[:title]}
+#   end
+
+#   def void_last_transaction
+#       @total -= @cart.last[:price] * @cart.last[:quantity]
+#   end
+# end
